22b3f7cb5de1f69d434e2df56b6d6e3503745a93
subham <subham.bhattacharjee1@gmail.com>
2018-07-02 01:14:19 +0530

    Added proper self-referential relations field

--- backend/djreact/todos/api.py
+++ backend/djreact/todos/api.py
@@ -6,22 +6,10 @@ from .models import Todo
 
 class TodoResource(ModelResource):
     subtask = fields.ToManyField(
-        "todos.api.SubtakResource", 'subtask',
-        related_name='subtask', full=True,
-        null=True, blank=True
+        "self", 'subtask',
     )
 
     class Meta:
         queryset = Todo.objects.all()
         resource_name = 'todo'
         authorization = Authorization()
-
-
-class SubtakResource(ModelResource):
-    parent = fields.ForeignKey(
-        "todo.api.TodoResource", 'parent',
-        use_in='detail', null=True, blank=True
-    )
-
-    class Meta:
-        queryset = Todo.objects.all()
