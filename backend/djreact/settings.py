e0649e0d029bfc5ce15357e96f487034095d61af
subham <subham.bhattacharjee1@gmail.com>
2018-07-02 15:51:02 +0530

    added cors support

--- backend/djreact/settings.py
+++ backend/djreact/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'todos',
     'tastypie',
     'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'urls'
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
