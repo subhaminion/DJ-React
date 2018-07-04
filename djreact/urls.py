25dd68da558d669dff22ee0151c0382066732d13
subham <subham.bhattacharjee1@gmail.com>
2018-07-05 02:27:51 +0530

    changed url

--- djreact/urls.py
+++ djreact/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from todos.views import index
 
 urlpatterns = [
-	url(r'^lol/', index),
+	url(r'^index/', index),
     # url(r'^celerycheck/', celerycheck),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('todos.urls')),
